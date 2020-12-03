c2c7493e9f82df68f2426111949d7128b4713a3b
nizam9 <nizamuddin407@gmail.com>
2020-12-03 13:12:11 +0530

    update cred

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-	//mongoURI: 'mongodb+srv://nizam:Nizam@123@cluster-i-mrquj.mongodb.net/littleHarbor?retryWrites=true&w=majority',
 	mongoURI: 'mongodb://localhost:27017/bharosaa',
 	cookieKey: 'apexLegendBlaze',
 	jwt_secret_key : 'SecretKeyHere',
